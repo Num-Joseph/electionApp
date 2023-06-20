580ec15243e06382931f2dd69904d002c17580d4
Num - Joseph <konum5437@gmail.com>
2023-06-20 16:03:10 -0700

    uploading my repo

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
-
 const appRoutes = require("./routes/index");
 app.use(bodyparser.json());
 app.use("/api", appRoutes);
@@ -10,7 +9,7 @@ app.use("/api", appRoutes);
 app.get("/", (req, res, next) => {
   res.status(200).json({
     message: "welcome to our  voting app",
-  });  
+  }); 
 });
 app.listen(PORT, ( ) => {
   console.log(`server running on port${PORT}`);
