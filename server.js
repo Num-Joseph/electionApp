ab5299f0fa153d28456a96da876db223440b224e
Num - Joseph <konum5437@gmail.com>
2023-06-20 16:00:20 -0700

    uploading my repo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyparser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const appRoutes = require("./routes/index.js");
+const appRoutes = require("./routes/index");
 app.use(bodyparser.json());
 app.use("/api", appRoutes);
 
